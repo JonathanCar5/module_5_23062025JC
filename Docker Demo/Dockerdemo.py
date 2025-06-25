86c791231f73c3bc3f56602e6592252d7bb1442c
jonathancarroll-qa <jonathan.carroll@stockton.gov.uk>
2025-06-25 13:18:33 +0100

    Updated Docker

---
+++ Docker Demo/Dockerdemo.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+print('hello world, I just ran a docker!')
+
+print(np.array([1,2,3,4]))
+
+print('I just ran docker twice')
--- python_app/myfirstPythonFile.py
+++ python_app/myfirstPythonFile.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
 # Load your data into a DataFrame
-df = pd.read_csv('C:/Users/Admin/Desktop/module_5_23062025JC/python_app/03_Library SystemCustomers.csv')
+df = pd.read_csv('03_Library SystemCustomers.csv')
 
 #drop na
-df = df.dropna(How='all')
+df = df.dropna(how='all')
 
 df['Book checkout'] =pd.to_datetime(df['Book checkout'].str.rplace('"',''), errors='coerce')
 
@@ -18,6 +18,14 @@ print(df)
 
 print("\nFinal shape:", df.shape)
 
-df.to_csv("Cleaned_03_Library SystemBook.csv", index=False)
+df.to_csv("Cleaned_03_Library SystemCustomers.csv", index=False)
 
-print(f"Cleaned file saved as Cleaned_03_Library SystemBook.csv")
+print("Cleaned file saved as Cleaned_03_Library SystemCustomers.csv")
+
+import pandas as pd
+
+# Load the original CSV
+df = pd.read_csv("python_app\Cleaned_Books.csv")
+
+# Save to a new CSV
+df.to_csv("Cleaned_Books2.csv", index=False)
